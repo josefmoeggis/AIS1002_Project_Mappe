8e0b2fe998d05d89739c73971cd431b6e77fcf71
joelo <joeloe99@hotmail.com>
2023-03-26 18:01:01 +0200

    Have to restructure AirObject to include a mesh instead of inherit

--- AirObject.cpp
+++ AirObject.cpp
@@ -5,32 +5,32 @@
 #include "AirObject.hpp"
 
 // Setting length of stl file to know bounds and scale
-void AirObject::setLength(int length) {
+void AirObject::setLength(std::shared_ptr<int> length) {
     fileLength_ = length;
 }
 
 // Setting airspeed
-void AirObject::setAS(double airspeed) {
+void AirObject::setAS(std::shared_ptr<double> airspeed) {
     airspeed_ = airspeed;
 }
 
 // Setting lift coefficient
-void AirObject::setLiftCoefficient(float liftcoefficient) {
+void AirObject::setLiftCoefficient(std::shared_ptr<float> liftcoefficient) {
     liftCoefficient_ = liftcoefficient;
 }
 
 // Setting wing area
-void AirObject::setWingArea(float wingArea) {
+void AirObject::setWingArea(std::shared_ptr<float> wingArea) {
     wingArea_ = wingArea;
 }
 
-void AirObject::setAngleOfAttack(float AoA) {
+void AirObject::setAngleOfAttack(std::shared_ptr<float> AoA) {
     angleOfAttack_ = AoA;
 }
 
 // Calculat lift from different standards
-float AirObject::calculateLift() {
-    float lift = (1/2) * airDensity_ * std::pow(airspeed_, 2) * wingArea_ * liftCoefficient_;
+std::shared_ptr<float> AirObject::calculateLift() {
+    std::shared_ptr<float> lift = (1/2) * airDensity_ * std::pow(airspeed_, 2) * wingArea_ * liftCoefficient_;
     return lift;
 }
 
