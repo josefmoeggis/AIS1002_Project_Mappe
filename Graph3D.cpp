288164869d2ada0a728e66561ae2bcad60bfb8d3
joelo <joeloe99@hotmail.com>
2023-04-17 20:51:53 +0200

    Line finally fixed. Does not delete back vector segment when out of bounds

--- Graph3D.cpp
+++ Graph3D.cpp
@@ -24,13 +24,16 @@ int Graph3D::getDivisions() {
 
 void Graph3D::updateLineVectors(float lift, float resolution) {
     float stepSize = (float)*gridSize_ / resolution;
+
     if(graphVectors_->empty()) {
-        graphVectors_->emplace_back(Vector3 {grid_->position.x, -((float)*gridSize_ / 2), -((float)*gridSize_ / 2)});
+        graphVectors_->push_back(Vector3 {grid_->position.x, -((float)*gridSize_ / 2), -((float)*gridSize_ / 2)});
     }
-    if(graphVectors_->back().x >= ((float)*gridSize_ / 2)) {
+
+    graphVectors_->emplace_back(Vector3 {grid_->position.x, lift, graphVectors_->back().z + stepSize});
+
+    if(graphVectors_->back().x >= (*gridSize_ / 2)) {
         graphVectors_->erase(graphVectors_->begin());
     }
-    graphVectors_->emplace_back(Vector3 {grid_->position.x, lift, graphVectors_->back().z + stepSize});
 }
 
 std::vector<Vector3> Graph3D::getVectors() {
