76e00e318cfaf3328ccbf170007b30104ef40f83
joelo <joeloe99@hotmail.com>
2023-05-13 19:16:31 +0200

    Start

--- tests/test_object.cpp
+++ tests/test_object.cpp
@@ -1,8 +1,8 @@
 //
 // Created by joelo on 17.04.2023.
 //
+#define CATCH_CONFIG_MAIN
 #include "include/AirObject.hpp"
 
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <Catch.>
 
